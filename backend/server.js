7b0104912675f5c82cef0e1bb98fede573d1af25
aginamena5 <aginamena5@gmail.com>
2022-05-02 14:40:44 -0400

    user can login and logout

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,15 @@ const express = require("express");
 //creating an express application
 const app = express();
 
+// we can to be able to take incoming request from the client. 
+// we use cors for that
+const cors = require("cors");
+
+//enabling cors in the app
+app.use(cors());
+
 //routing for the home page
-app.get("/", (req, res) => res.send("Hello world"))
+app.get("/", (req, res) => res.send({ title: "hello world" }))
 
 //running the express app on localhost 9000
 app.listen(9000, () => console.log("App is running on part 9000"))
